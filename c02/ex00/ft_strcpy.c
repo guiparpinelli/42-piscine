63c4179af8c46a6ee975b3468d7a5d396b7e65ae
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-03-17 11:41:27 +0000

    finish ex00

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -1,19 +1,39 @@
-char *ft_strcpy(char *dest, char *src) {
-  int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gparpine <gparpine@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 17:43:49 by gparpine          #+#    #+#             */
+/*   Updated: 2023/03/16 18:04:51 by gparpine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-  i = 0;
-  while (*src != '\0') {
-    *(dest + i++) = *src++;
-  }
-  *(dest + i) = '\0';
-  return dest;
+/*
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src);
+
+int	main(void)
+{
+	char	src[] = "some string";
+	char	dest[] = "";
+
+	ft_strcpy(dest, src);
+	printf("%s\n", dest);
+	return (0);
 }
-/* #include <stdio.h> */
+*/
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
 
-/* int main() { */
-/*   char src[] = "some string"; */
-/*   char dest[] = ""; */
-
-/*   ft_strcpy(dest, src); */
-/*   printf("%s\n", dest); */
-/* } */
+	i = 0;
+	while (*src != '\0')
+	{
+		*(dest + i++) = *src++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
