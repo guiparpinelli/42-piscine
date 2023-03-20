ab9a641df7971dd22ceb384d854f161e31b93f8b
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-03-20 19:04:21 +0000

    finish ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char *ft_strlowcase(char *str);
+
+int main(int argc, char *argv[]) {
+  char test[] = "LOWERCASE ME";
+  char test_empty[] = "";
+  char test_upper_only[] = "already lower case";
+
+  printf("%s\n", ft_strlowcase(test));
+  printf("%s\n", ft_strlowcase(test_empty));
+  printf("%s\n", ft_strlowcase(test_upper_only));
+  return 0;
+}
+
+char *ft_strlowcase(char *str) {
+  int i;
+
+  i = 0;
+  while (str[i]) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      str[i] += 32;
+    }
+    i++;
+  }
+  return (str);
+}
