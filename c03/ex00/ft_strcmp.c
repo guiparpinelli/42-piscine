61b32bf07c025289fe99e4f3cf0dccd95df309a0
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-03-21 11:18:50 +0000

    finish ex00

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+#include <string.h>
+
+int ft_strcmp(char *s1, char *s2);
+
+int main(int argc, char *argv[]) {
+
+  if (argc == 1 || argc > 3) {
+    return 1;
+  }
+
+  printf("strcmp: %d\n", strcmp(argv[1], argv[2]));
+  printf("ft_strcmp: %d\n", ft_strcmp(argv[1], argv[2]));
+  return 0;
+}
+*/
+int ft_strcmp(char *s1, char *s2) {
+  int i;
+
+  i = 0;
+  while (s1[i] == s2[i]) {
+    if (s1[i] == '\0') {
+      return 0;
+    }
+    i++;
+  }
+  return s1[i] - s2[i];
+}
