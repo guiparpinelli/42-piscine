0cd50d44c5301cfbac8df702e19e86dae4ddc273
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-03-27 22:50:11 +0100

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 echo "Enter the evaluated project path:"
-# read -r eval_path
-eval_path="/Users/parpi/42/_testdir/eval_c00"
+read -r eval_path
 
 echo "Running norminette checks..."
-#norminette "$eval_path" -R CheckForbiddenSourceHeader
+norminette "$eval_path" -R CheckForbiddenSourceHeader
 
 echo "Enter a path to search for main.c in its subdirectories:"
-# read -r path
-path="/Users/parpi/42/_testdir/c00"
+read -r path
 
 # Copy all `main.c` files in "$path" subdirectories onto evaluated project subdirectories path.
 find "$path" -type d -exec sh -c '
@@ -23,13 +21,12 @@ find "$path" -type d -exec sh -c '
   done
 ' sh "$eval_path" {} +
 
-# TODO fix this loop
 # Loop through all subdirectories in the evaluated project path and compile `*.c` files into user_exe
-for dir in "$eval_path"/*; do
-    echo "testing $dir"
-    if [ -n "$(find "$dir" -maxdepth 1 -type f -name "*.c" -print -quit)" ]; then
-        echo "Compiling *.c files in $dir to user_exe"
-        cc -Wall -Wextra -Werror "$dir"/*.c -o user_exe
-        read -p "Press enter to continue or Ctrl+C to abort" -n 1 -r
-    fi
+for dir in "$eval_path"/*/; do
+  echo "testing $dir"
+  if [ -n "$(find "$dir" -maxdepth 1 -type f -name "*.c" -print -quit)" ]; then
+    echo "Compiling *.c files in $dir to user_exe"
+    # cc -Wall -Wextra -Werror "$dir"*.c -o user_exe
+    read -p "Press enter to continue or Ctrl+C to abort" -n 1 -r
+  fi
 done
