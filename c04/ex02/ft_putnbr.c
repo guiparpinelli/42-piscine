71966e2bc7269fbb1bd4c750eebc576cb1967bfe
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-03-27 11:44:52 +0100

    fix ft_putnbr

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -15,17 +15,11 @@
 void ft_putchar(char c) { write(1, &c, 1); }
 
 void ft_putnbr(int nb) {
-  if (nb == -2147483648) {
-    ft_putchar('-');
-    ft_putchar('2');
-    ft_putnbr(147483648);
-  } else if (nb < 0) {
-    ft_putchar('-');
-    nb = -nb;
-    ft_putnbr(nb);
-  } else if (nb > 9) {
+  if (nb < 0) {
+    write(1, "-", 1);
+    nb *= -1;
+  }
+  if (nb > 9)
     ft_putnbr(nb / 10);
-    ft_putnbr(nb % 10);
-  } else
-    ft_putchar(nb + 48);
+  ft_putchar(nb % 10 + '0');
 }
--- c04/ex02/main.c
+++ c04/ex02/main.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 void ft_putnbr(int nb);
 
-int main(int argc, char *argv[]) {
-
-  if (argc == 1 || argc > 2)
-    return 1;
-
-  int num = atoi(argv[1]);
-  ft_putnbr(num);
-  return 0;
+int main(int ac, char **av) {
+  if (ac == 2) {
+    int num = atoi(av[1]);
+    ft_putnbr(num);
+  }
+  write(1, "\n", 1);
 }
